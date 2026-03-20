019b8fa9b3330cb69541deaeedcc57bc5b72c8c4
Satyam Narayan <satyam.narayan.silversky@gmail.com>
2026-03-20 19:33:48 +0530

    Added projects

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,16 +3,45 @@ import ff1 from './projects/familyFeed/ff1.png';
 import ff2 from './projects/familyFeed/ff2.png';
 import ff3 from './projects/familyFeed/ff3.png';
 import ff4 from './projects/familyFeed/ff4.png';
+
 import guCover from './projects/guisedup/cover.png';
 import gu1 from './projects/guisedup/gu1.png';
 import gu2 from './projects/guisedup/gu2.png';
 import gu3 from './projects/guisedup/gu3.png';
 import gu4 from './projects/guisedup/gu4.png';
+
 import frCover from './projects/footrank/cover.png';
 import fr1 from './projects/footrank/fr1.png';
 import fr2 from './projects/footrank/fr2.png';
 import fr3 from './projects/footrank/fr3.png';
 import fr4 from './projects/footrank/fr4.png';
+
+import eqCover from './projects/eaziQuote/cover.png';
+import eq1 from './projects/eaziQuote/eq1.png';
+import eq2 from './projects/eaziQuote/eq2.png';
+import eq3 from './projects/eaziQuote/eq3.png';
+import eq4 from './projects/eaziQuote/eq4.png';
+import eq5 from './projects/eaziQuote/eq5.png';
+import eq6 from './projects/eaziQuote/eq6.png';
+import eq7 from './projects/eaziQuote/eq7.png';
+import eq8 from './projects/eaziQuote/eq8.png';
+
+import tuCover from './projects/tuck/cover.png';
+import tu1 from './projects/tuck/tu1.png';
+import tu2 from './projects/tuck/tu2.png';
+import tu3 from './projects/tuck/tu3.png';
+import tu4 from './projects/tuck/tu4.png';
+import tu5 from './projects/tuck/tu5.png';
+import tu6 from './projects/tuck/tu6.png';
+import tu7 from './projects/tuck/tu7.png';
+import tu8 from './projects/tuck/tu8.png';
+import tu9 from './projects/tuck/tu9.png';
+import tu10 from './projects/tuck/tu10.png';
+import tu11 from './projects/tuck/tu11.png';
+import tu12 from './projects/tuck/tu12.png';
+import tu13 from './projects/tuck/tu13.png';
+import tu14 from './projects/tuck/tu14.png';
+
 
 
 export const Images = {
@@ -36,5 +65,33 @@ export const Images = {
         fr2,
         fr3,
         fr4,
-    }
+    },
+    eaziQuote: {
+        cover: eqCover,
+        eq1,
+        eq2,
+        eq3,
+        eq4,
+        eq5,
+        eq6,
+        eq7,
+        eq8
+    },
+    tuck: {
+        cover: tuCover,
+        tu1,
+        tu2,
+        tu3,
+        tu4,
+        tu5,
+        tu6,
+        tu7,
+        tu8,
+        tu9,
+        tu10,
+        tu11,
+        tu12,
+        tu13,
+        tu14,
+    },
 };
